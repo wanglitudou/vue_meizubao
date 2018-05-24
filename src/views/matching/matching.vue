0840203c87cc6a3a19abb7bd37924269ea094a38
hydraZhang <670688667@qq.com>
2018-05-24 15:46:15 +0800

    详情页修改

--- src/views/matching/matching.vue
+++ src/views/matching/matching.vue
@@ -53,7 +53,47 @@
     </div>
 </template>
 <script>
-export default {};
+export default {
+
+  mounted(){
+    this.init();
+  },
+  methods: {
+    init(){
+      Date.prototype.Format = function (fmt) { //author: meizz
+        var o = {
+          "M+": this.getMonth() + 1, //月份
+          "d+": this.getDate(), //日
+          "h+": this.getHours(), //小时
+          "m+": this.getMinutes(), //分
+          "s+": this.getSeconds(), //秒
+          "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+          "S": this.getMilliseconds() //毫秒
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+          if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+      }
+
+      this.$axios
+        .get(window.ajaxSrc + "/api/meizubao/instrumentDetail", {
+          params: {'id': 6}
+        })
+        .then(res => {
+          console.log(res);
+          if (res.data.status_code == 1001) {
+            this.data = res.data.data;
+          }
+        })
+        .catch(() => {
+          console.log("http请求错误");
+        });
+    },
+
+
+  },
+};
 </script>
 <style scoped>
 .container {
