b21c2aee24e0553943287abf827985a6a2210326
wanglitudou <1626408397@qq.com>
2018-06-06 10:15:07 +0800

    提交代码

--- src/views/authname/authname.vue
+++ src/views/authname/authname.vue
@@ -175,7 +175,6 @@ export default {
         data: formData,
         processData: false,
         contentType: false,
-
         dataType: "json",
         success: function(res) {
           console.log(res);
@@ -183,13 +182,14 @@ export default {
             that.fileList = res.data;
           }
           if (num == 1) {
-            that.imgOne = res.data.url;
+            that.data.imgOne = res.data.url;
+            console.log("666");
           } else if (num == 2) {
-            that.imgtwo = res.data.url;
+            that.data.imgtwo = res.data.url;
           } else if (num == 3) {
-            that.imgthree = res.data.url;
+            that.data.imgthree = res.data.url;
           } else if (num == 4) {
-            that.imgfour = res.data.url;
+            that.data.imgfour = res.data.url;
           }
         },
         error: function(res) {
