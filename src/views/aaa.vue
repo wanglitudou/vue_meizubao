4ad298c03ac5199bd94dfa1605a18d996c6c1f7d
wanglitudou <1626408397@qq.com>
2018-06-28 11:10:54 +0800

    sss

--- src/views/aaa.vue
+++ src/views/aaa.vue
@@ -64,7 +64,7 @@ export default {
               sizeType: ["original", "compressed"], // 可以指定是原图还是压缩图，默认二者都有
               sourceType: ["album", "camera"], // 可以指定来源是相册还是相机，默认二者都有
               success: function(res) {
-                alert(res);
+                alert(res.localIds);
                 var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
                 console.log(localIds + "666");
                 console.log("微信上传");
